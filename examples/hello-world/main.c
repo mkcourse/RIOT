a134fdaf738c9a3f11558e800c74b3fdcc7e928f
Vladimir <martya-vladimir@yandex.ru>
2018-04-07 15:38:13 +0300

    07.04 cego-to ne xvataet

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -22,16 +22,45 @@
 #include <stdio.h>
 
 #include "periph/gpio.h"
+
+#include "periph/pm.h"
+
 #include "rtctimers-millis.h"
+#include "thread.h"
+#include "shell.h"
+#include "shell_commands.h"
+
+
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+static kernel_pid_t process_pid;
+static msg_t process_msg;
+
+#define  MY_PROCESS_STACK_SIZE (1024)
+
 
 uint32_t milliseconds_last_press = 0;
 uint32_t milliseconds_last_blink = 0;
 
-static void btn_led_toggle(void*arg)
+static void *process_thread(void *arg){
+	(void)arg;
+	msg_t message;
+	
+	gpio_set(GPIO_PIN(PORT_B, 0));
+	while(1){
+		msg_receive(&message);
+		gpio_toggle(GPIO_PIN(PORT_B, 0));
+		DEBUG("LED set to %d\n", gpio_read(GPIO_PIN(PORT_B, 0)) );
+	}
+	return NULL;
+}
+
+static void btn_led_toggle(void *arg)
 {
 		(void)arg;
 		milliseconds_last_blink = rtctimers_millis_now();
-		int k=0;
+		/* int k=0;
 		if(rtctimers_millis_now() - milliseconds_last_press > 100)
 		{
 			for(int i=0; i<6;i++){
@@ -44,12 +73,21 @@ static void btn_led_toggle(void*arg)
 			}
 			k=0;
 			}
-			milliseconds_last_press = rtctimers_millis_now();
-		}
+			milliseconds_last_press = rtctimers_millis_now(); */
+			if(rtctimers_millis_now() - milliseconds_last_press > 100)
+    {
+      msg_send (&process_msg, process_pid);
+      milliseconds_last_press = rtctimers_millis_now();
+    }
+		
 }
 
 int main(void)
 {
+	
+	pm_init();
+	pm_prevent_sleep = 1;
+	
 	rtctimers_millis_init();
     puts("Hello World!");
 	
@@ -61,6 +99,13 @@ int main(void)
 	
 	gpio_set(GPIO_PIN(PORT_B, 0));
 	gpio_init_int (GPIO_PIN(PORT_B,1),GPIO_IN_PU,GPIO_FALLING,btn_led_toggle,NULL);
+	
+	char stack[MY_PROCESS_STACK_SIZE];
+	process_pid = thread_create(stack, MY_PROCESS_STACK_SIZE,THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,process_thread, NULL, "our process");
+	
+	//shell init
+	char line_buf 
+	
 	
     return 0;
 }
